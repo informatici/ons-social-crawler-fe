d9208349a3fb082820bbfd8f18645dc5ffef7e23
Gianpaolo <garbarino@digiunit.it>
2023-03-22 20:22:46 +0100

    Aggiunto chiamata twitter, axios e env

--- ons-social-crawler-fe/src/views/NavItemView.vue
+++ ons-social-crawler-fe/src/views/NavItemView.vue
@@ -2,11 +2,25 @@
 import {useRoute} from "vue-router";
 const route = useRoute()
 // const props = defineProps(["pageTitle"])
+import ApiService from "../core/services/ApiService";
+import {onMounted, ref} from "vue";
+
+const twitter = ref([])
+
+const init = async () => {
+  const res = await ApiService.get('/twitter/simple')
+  twitter.value = res.data
+}
+
+onMounted(async () => {
+  await init()
+})
 
 </script>
 <template>
   <main class="page-container">
     <h1>{{ route?.meta?.label }}</h1>
+    <div v-for="(twit, ix) in twitter" :key="ix">{{ twit.name }}</div>
   </main>
 </template>
 <style lang="scss">
