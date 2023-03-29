12029d4217f6aaed7f2d4461ef0a39f09cb225c4
Gianpaolo <garbarino@digiunit.it>
2023-03-29 10:30:32 +0200

    Ok

--- ons-social-crawler-fe/src/router/index.js
+++ ons-social-crawler-fe/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/DashboardView.vue'
+import { useAuthStore } from "../stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,35 +20,142 @@ const router = createRouter({
     // },
     {
       path: '/',
-      name: 'dashboard',
-      component: () => import('../views/DashboardView.vue'),
-      label: 'Dashboard',
+      redirect: "/dashboard",
+      component: () => import('../layouts/main-layout/MainLayout.vue'),
+      meta: {
+        middleware: "auth",
+      },
+      children: [
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {label: 'Dashboard',},
+          // label: 'Dashboard'
+        },
+        {
+          path: '/youTube',
+          name: 'youTube',
+          component: () => import('../views/NavItemView.vue'),
+          meta: {label: 'YouTube',},
+        },
+        {
+          path: '/twitter',
+          name: 'twitter',
+          component: () => import('../views/NavItemView.vue'),
+          meta: {label: 'Twitter',},
+        },
+        {
+          path: '/twitch',
+          name: 'twitch',
+          component: () => import('../views/NavItemView.vue'),
+          meta: {label: 'Twitch',},
+        },
+        // {
+        //   path: '/login',
+        //   name: 'login',
+        //   component: () => import('../views/LoginView.vue'),
+        // },
+      ],
     },
     {
-      path: '/youTube',
-      name: 'youTube',
-      component: () => import('../views/NavItemView.vue'),
-      meta: {label: 'YouTube',},
+      path: "/",
+      component: () => import("../layouts/AuthLayout.vue"),
+      // component: () => import("@/views/LoginView.vue"),
+      children: [
+        {
+          path: "/login",
+          name: "login",
+          component: () =>
+              import("../views/LoginView.vue"),
+          meta: {
+            pageTitle: "Sign In",
+          },
+        },
+        // {
+        //   path: "/sign-up",
+        //   name: "sign-up",
+        //   component: () =>
+        //       import("@/views/crafted/authentication/basic-flow/SignUp.vue"),
+        //   meta: {
+        //     pageTitle: "Sign Up",
+        //   },
+        // },
+        // {
+        //   path: "/password-reset",
+        //   name: "password-reset",
+        //   component: () =>
+        //       import("@/views/crafted/authentication/basic-flow/PasswordReset.vue"),
+        //   meta: {
+        //     pageTitle: "Password reset",
+        //   },
+        // },
+      ],
     },
-    {
-      path: '/twitter',
-      name: 'twitter',
-      component: () => import('../views/NavItemView.vue'),
-      meta: {label: 'Twitter',},
-    },
-    {
-      path: '/twitch',
-      name: 'twitch',
-      component: () => import('../views/NavItemView.vue'),
-      meta: {label: 'Twitch',},
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue'),
-    },
+
+    // {
+    //   path: "/",
+    //   component: () => import("@/layouts/SystemLayout.vue"),
+    //   children: [
+    //     {
+    //       // the 404 route, when none of the above matches
+    //       path: "/404",
+    //       name: "404",
+    //       component: () => import("@/views/crafted/authentication/Error404.vue"),
+    //       meta: {
+    //         pageTitle: "Error 404",
+    //       },
+    //     },
+    //     {
+    //       path: "/500",
+    //       name: "500",
+    //       component: () => import("@/views/crafted/authentication/Error500.vue"),
+    //       meta: {
+    //         pageTitle: "Error 500",
+    //       },
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/:pathMatch(.*)*",
+    //   redirect: "/404",
+    // },
+
 
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  // const configStore = useConfigStore();
+
+  // current page view title
+  // document.title = `${to.meta.pageTitle} - ${import.meta.env.VITE_APP_NAME}`;
+
+  // reset config to initial state
+  // configStore.resetLayoutConfig();
+
+  // verify auth token before each page change
+  //  authStore.verifyAuth();
+
+  // before page access check if page requires authentication
+  if (to.meta.middleware == "auth") {
+    if (authStore.isAuthenticated) {
+      next();
+    } else {
+      next({ name: "login" });
+    }
+  } else {
+    next();
+  }
+  // next();
+
+  // Scroll page to top on every route change
+  // window.scrollTo({
+  //   top: 0,
+  //   left: 0,
+  //   behavior: "smooth",
+  // });
+});
 
 export default router
