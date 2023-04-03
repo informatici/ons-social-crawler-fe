dcb7523bab73462d1cde22b445fa7092d0839b4a
Davide Bertuzzi <bertuzzi@digiunit.it>
2023-04-03 17:56:16 +0200

    Commit: merge

--- ons-social-crawler-fe/src/views/NavItemView.vue
+++ ons-social-crawler-fe/src/views/NavItemView.vue
@@ -2,13 +2,13 @@
 import { useRoute } from 'vue-router'
 const route = useRoute()
 // const props = defineProps(["pageTitle"])
-import ApiService from "../core/services/ApiService";
-import {onMounted, ref} from "vue";
+import ApiService from '../core/services/ApiService'
+import { onMounted, ref, computed } from 'vue'
 
 const twitter = ref([])
 
 const init = async () => {
-  const res = await ApiService.get('/twitter/simple')
+  const res = await ApiService.get('/twitter/elasticsearch/twits')
   twitter.value = res.data
 }
 
@@ -16,6 +16,9 @@ onMounted(async () => {
   await init()
 })
 
+const total = computed(() => {
+  return twitter.value?.hits?.total?.value ?? 0
+})
 </script>
 <template>
   <main class="page-container">
