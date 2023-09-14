a84214f16c649856239857c7480a3ad9a9229387
mwithi <alessandro.domanico@yahoo.it>
2023-09-14 19:39:37 +0200

    DateRange controller

--- ons-social-crawler-fe/src/views/DashboardView.vue
+++ ons-social-crawler-fe/src/views/DashboardView.vue
@@ -6,12 +6,14 @@ const route = useRoute()
 
 <script>
 import transactions from './data/transactions.js';
+import DateRange from "./components/DateRange.vue";
 import LatestTransactionsChart from "./components/LatestTransactionsChart.vue";
 import TransactionBreakdownChart from "./components/TransactionBreakdownChart.vue";
 export default {
   name: 'dashboard',
   // Register the components to be used
   components: {
+    DateRange,
     LatestTransactionsChart,
     TransactionBreakdownChart,
   },
@@ -19,10 +21,9 @@ export default {
     return {
       transactions,
       range: {
-        start: new Date('2019-09-15T13:24:15.000Z'),
-        end: new Date() 
+        start: new Date(new Date().setHours(0,0,0,0)),
+        end: new Date(new Date().setHours(24,0,0,0))
       },
-      
     }
   },
   computed: {
@@ -34,6 +35,12 @@ export default {
         );
       });
     },
+  },
+  methods: {
+    updateRange(range) {
+      // console.log(range);
+      this.range = range;
+    }
   }
 }
 </script>
@@ -46,18 +53,16 @@ export default {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
             <path
-              d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z"
-            />
+              d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z" />
           </svg>
         </span>
         {{ route?.meta?.label }}
       </h1>
     </div>
-    <header>
-      <h4>Date Range</h4>
-      <v-date-picker mode="range" v-model.range="range"/>
-    </header>
     <div>
+      <div>
+        <DateRange :entries="range" @rangeChanged="updateRange" />
+      </div>
       <div>
         <LatestTransactionsChart :entries="filteredTransactions" />
       </div>
@@ -69,7 +74,7 @@ export default {
 </template>
 
 <style lang="scss">
-.settings-view {
+.dashboard-view {
   .section {
     margin-bottom: 3.2rem;
 
@@ -93,18 +98,15 @@ export default {
   }
 
   @media screen and (max-width: 1400px) {
-    .section {
-    }
+    .section {}
   }
 
   @media screen and (max-width: 992px) {
-    .section {
-    }
+    .section {}
   }
 
   @media screen and (max-width: 768px) {
-    .section {
-    }
+    .section {}
   }
 }
 </style>
