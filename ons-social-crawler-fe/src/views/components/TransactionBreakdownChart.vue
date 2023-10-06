71c9c6d427a5fd7f2e9579d9200336880c8a65b8
mwithi <alessandro.domanico@yahoo.it>
2023-10-06 21:35:25 +0200

    Simplified sorting

--- ons-social-crawler-fe/src/views/components/TransactionBreakdownChart.vue
+++ ons-social-crawler-fe/src/views/components/TransactionBreakdownChart.vue
@@ -32,8 +32,7 @@ export default {
       // Manually reorder the result array
       result.sort((a) => {
         if (a.text === 'isHate = true') return 1;
-        if (a.text === 'isHate = false') return -1;
-        return 0;
+        return -1;
       });
 
       return result
