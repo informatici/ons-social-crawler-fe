58c50da410a6380c159820d1c2de24c1560c75a3
Davide Bertuzzi <bertuzzi@digiunit.it>
2024-01-21 23:55:39 +0100

    TAG: dev-fe-202401212255

--- ons-social-crawler-fe/src/views/SettingsView.vue
+++ ons-social-crawler-fe/src/views/SettingsView.vue
@@ -200,15 +200,9 @@ onMounted(async () => {
           :init="{
             toolbar_mode: 'sliding',
             plugins:
-              'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+              'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount code',
             toolbar:
-              'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            mergetags_list: [
-              { value: 'First.Name', title: 'First Name' },
-              { value: 'Email', title: 'Email' }
-            ],
+              'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
             language: 'it'
           }"
           @init="onEditorReady"
